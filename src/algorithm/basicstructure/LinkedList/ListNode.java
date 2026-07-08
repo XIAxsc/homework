92d4fb21070345e82c9281bda1c934ccff32d411
XIAxsc <1743317984@qq.com>
2026-07-08 21:51:51 +0800

    day26

--- src/algorithm/basicstructure/LinkedList/ListNode.java
+++ src/algorithm/basicstructure/LinkedList/ListNode.java
@@ -2,7 +2,13 @@ package algorithm.basicstructure.LinkedList;
 
 
 /**
- * 自定义单链表
+ *  自定义单链表
+ * <p>
+ *  createLinkedList(int[] arr)                             传入一个数组，创建单链表
+ *  traverse(ListNode head)                                 传入链表的一个结点，从该结点开始依次向后遍历
+ *  insertTail(ListNode head, int value)                    传入链表头结点和一个int类型值value，在链表结尾新增值为value的结点
+ *  insertMiddle(ListNode head, int value, int index)       在第index节点之后插入值为value的新结点
+ *
  */
 public class ListNode {
     public int val;             // 值
@@ -12,7 +18,9 @@ public class ListNode {
         this.val = x;
     }
 
-    // 传入数组，创建单链表
+    /**
+     * 传入数组，创建单链表
+     */
     public static ListNode createLinkedList(int[] arr) {
         // 如果arr不指向数组，或者数组为空，创建单链表失败（边界条件判定）
         if (arr == null || arr.length == 0) {
@@ -32,7 +40,9 @@ public class ListNode {
         return head;
     }
 
-    // 遍历单链表
+    /**
+     * 遍历单链表
+     */
     public static void traverse(ListNode head) {
         System.out.println("遍历当前单链表：");
         // 创建p指针指向头结点，挨个向后遍历链表节点并打印节点value
@@ -42,7 +52,13 @@ public class ListNode {
         System.out.println();
     }
 
-    // 在单链表表尾插入新的节点
+
+    /**
+     * 在单链表表尾插入新的节点
+     *
+     * @param head  头结点
+     * @param value 新增结点的值
+     */
     public static void insertTail(ListNode head, int value) {
         // 指针p指向头结点
         ListNode p = head;
@@ -54,7 +70,13 @@ public class ListNode {
         p.next = new ListNode(value);
     }
 
-    // 在第index节点后插入节点
+    /**
+     * 在第index节点后插入节点
+     *
+     * @param head  头结点
+     * @param value 新增结点的值
+     * @param index 新增结点的前一个位置
+     */
     public static void insertMiddle(ListNode head, int value, int index) {
         ListNode p = head;
         // 循环找到index节点
